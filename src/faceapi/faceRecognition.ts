2aeeaff76386aa4090f13d085ce32d09ac95b31e
yokesh <yokeshthirumoorthi@gmail.com>
2019-06-24 04:54:08 -0700

    include face recognition

---
+++ src/faceapi/faceRecognition.ts
@@ -0,0 +1,40 @@
+import * as faceapi from 'face-api.js';
+
+import { canvas, faceDetectionNet, faceDetectionOptions, saveFile } from './commons';
+const rabbit = require('../rabbitmq/receive');
+const pg = require('../database/postgres');
+const fs = require('fs');
+
+
+
+export async function recognize(referenceImagePath, queryImagePath) {
+
+  const REFERENCE_IMAGE = referenceImagePath;
+  const QUERY_IMAGE = queryImagePath;
+
+  await faceDetectionNet.loadFromDisk('./weights')
+  await faceapi.nets.faceLandmark68Net.loadFromDisk('./weights')
+  await faceapi.nets.faceRecognitionNet.loadFromDisk('./weights')
+
+  const referenceImage = await canvas.loadImage(REFERENCE_IMAGE)
+  const queryImage = await canvas.loadImage(QUERY_IMAGE)
+
+  const resultsRef = await faceapi.detectAllFaces(referenceImage, faceDetectionOptions)
+    .withFaceLandmarks()
+    .withFaceDescriptors()
+
+  const resultsQuery = await faceapi.detectAllFaces(queryImage, faceDetectionOptions)
+    .withFaceLandmarks()
+    .withFaceDescriptors()
+
+  const faceMatcher = new faceapi.FaceMatcher(resultsRef)
+
+  return faceMatcher;
+}
+
+rabbit.receiveMessage((photo_id) => {
+  pg.getPhotoFullPath(photo_id).then(async (imagePath) => {
+    const recognitionJson=  await recognize(imagePath, imagePath);
+    console.log(recognitionJson);
+  })
+});
