93c68120ff4dd3edb53fbc35863b72f4a45f35a3
yokesh <yokeshthirumoorthi@gmail.com>
2019-07-15 15:24:43 -0700

    update db schema

--- src/dbmigrateservice/migrations/2019-06-19-230146_create_photos/down.sql
+++ src/dbmigrateservice/migrations/2019-06-19-230146_create_photos/down.sql
@@ -1,6 +1,8 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE albums;
 DROP TABLE photos_exif;
-DROP TABLE face_descriptors;
-DROP TABLE photos;
 DROP TABLE exif;
+DROP TABLE face_descriptors;
+DROP TABLE faces;
+DROP TABLE bounding_boxes;
+DROP TABLE photos;
