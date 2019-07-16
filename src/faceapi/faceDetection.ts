6607059f2dd661b9ceb85adeaf34d13293716065
yokesh <yokeshthirumoorthi@gmail.com>
2019-07-15 22:11:06 -0700

    add faceDetection file

---
+++ src/faceapi/faceDetection.ts
@@ -0,0 +1,20 @@
+import * as faceapi from 'face-api.js';
+
+import { canvas, faceDetectionNet, faceDetectionOptions, saveFile } from './commons';
+
+async function run() {
+
+  await faceDetectionNet.loadFromDisk('./weights')
+
+  const img = await canvas.loadImage('./images/bbt1.jpg')
+  const detections = await faceapi.detectAllFaces(img, faceDetectionOptions)
+  console.log(detections);
+
+  const out = faceapi.createCanvasFromMedia(img) as any
+  faceapi.draw.drawDetections(out, detections)
+
+  saveFile('faceDetection.jpg', out.toBuffer('image/jpeg'))
+  console.log('done, saved results to out/faceDetection.jpg')
+}
+
+run()
