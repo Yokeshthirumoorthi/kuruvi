6e7327baf6283a1f94a5f6d22481ead8a4e77ea3
yokesh <yokeshthirumoorthi@gmail.com>
2019-07-18 15:10:25 -0700

    refactor imgproxy server code

--- src/imgproxyservice/index.js
+++ src/imgproxyservice/index.js
@@ -52,45 +52,91 @@ function _loadProto (path) {
   return grpc.loadPackageDefinition(packageDefinition);
 }
 
-/* ============================================================
-  Function: Download Image
-============================================================ */
-async function downloadImage (imagePath) {
-  const url = `http://caddy-fs:2015/${imagePath.album}/${imagePath.photo}`;
-  console.log("caddy url: ", url);
-  const signedUrlSegment = signature.getSignedImgURL(url);
-  const signedUrl = `http://imgproxy:8080${signedUrlSegment}`;
-
-  const response = await Axios({
-    url: signedUrl,
-    method: 'GET',
-    responseType: 'stream'
-  })
-
-  return response;
+/**
+ * Generates a proper caddy url using the album name
+ * and photo path.
+ */
+function getCaddyURL(photoFSDetails) {
+  const CADDY_PORT = 2015; //TODO: Use environmental variable
+  const CADDY_SERVICE = `caddy-fs`; //TODO: Use environmental variable
+  const album = photoFSDetails.album;
+  const photoPath = photoFSDetails.photo;
+  const URL =  `http://${CADDY_SERVICE}:${CADDY_PORT}/${album}/${photoPath}`;
+  return URL; 
 }
 
-async function resizeImage(call, callback) {
-  const ImgProxyRequest = call.request;
-  logger.info(`Received img resize request for photo: ${ImgProxyRequest.photo_id}`);
-  // get the photo_id from the Imgproxy request.
-  // retrieve the album path from the database.
-  // dwonload the photo in the path from the fs
-  // resize and save the photo
-  const getAlbumPhotoPathCallback = async (err, response) => {
-    const AlbumPhotoPathResponse = response;
-    console.log("AlbumPhotoPathResponse", AlbumPhotoPathResponse);
-    const download_response = await downloadImage(AlbumPhotoPathResponse);
-    await fs.saveFile(AlbumPhotoPathResponse, download_response);
-  };
+/**
+ * Generate a signed imgproxy URL for resizing images.
+ * The source image URL is a caddy URL.
+ */
+function getImgProxyPhotoResizeURL(caddyURL) {
+  const IMGPROXY_PORT = 8080; // TODO: Use environmental variable
+  const IMGPROXY_SERVICE = `imgproxy`; //TODO: Use environmental variable
+  const signedUrlSegment = signature.getSignedImgURL(caddyURL);
+  const URL = `http://${IMGPROXY_SERVICE}:${IMGPROXY_PORT}/${signedUrlSegment}`;
+  return URL;
+}
 
-  const AlbumPhotoPathRequest = ImgProxyRequest;
+/**
+ * Get image from imgProxy
+ */
+async function getImage(imgProxyURL) {
+  const image = await Axios({
+    url: imgProxyURL,
+    method: 'GET',
+    responseType: 'stream'
+  });
 
-  try {
-    client.getAlbumPhotoPath(AlbumPhotoPathRequest,getAlbumPhotoPathCallback);
-  } catch (err) {
-    logger.error(`Album photo path request failed: ${err}`);
+  return image; 
+}
+
+/**
+ * Save the image downloaded from imgproxy  
+ */
+async function saveImage(photoFSDetails, image) {
+  return fs.saveFile(photoFSDetails, image);
+}
+
+/**
+ * Given photo file system details,
+ * fetch the photo from caddy server 
+ * and resize it using imgproxy
+ */
+async function getResizedImage(photoFSDetails) {
+   const caddyURL = getCaddyURL(photoFSDetails);
+   const imgProxyURL = getImgProxyPhotoResizeURL(caddyURL);
+   const resizedImage = getImage(imgProxyURL);
+   return resizedImage;
+}
+
+/**
+ * Callback to be executed after getting photoFSDetails
+ * from postgresql grpc call
+ */
+async function resizeImageAndSave(err, response) {
+  if (err != null) {
+    logger.error("Error in PhotoFSDetails call");
   }
+
+  const photoFSDetails = response;
+  const resizedPhoto = await getResizedImage(photoFSDetails);
+  await saveImage(resizedPhoto);
+  logger.info(`Successfully resized and saved photo @ ${photoFSDetails.photo}`);
+}
+
+/**
+ * gRPC function implementation 
+ * get the photo_id from the Imgproxy request.
+ * Retrieve the album path from the database. 
+ * Download the photo in the path from fs, resize it and save it.
+ */
+function resizeImage(call, callback) { // TODO: Implement callback functionality
+  const photoId = call.request.photo_id; // TODO: change photo_id as photoId
+  logger.info(`Received img resize request for photo: ${photo_id}`);
+  const photoFSDetailsRequest = {
+    photo_id: photoId,
+  };
+  pgsql.getAlbumPhotoPath(photoFSDetailsRequest,resizeImageAndSave);
 }
 
 function cropFaces(call, callback) {
