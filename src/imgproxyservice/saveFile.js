269175c74a53c8b91ad879084c1abf3b6570e4e9
yokesh <yokeshthirumoorthi@gmail.com>
2019-07-19 00:52:44 -0700

    refactor fs

--- src/imgproxyservice/saveFile.js
+++ src/imgproxyservice/saveFile.js
@@ -11,19 +11,28 @@
 const fs = require('fs');
 const path = require('path');
 
-
-function createFolderIfNotExits(fullPath) {
-  if (!fs.existsSync(fullPath, { recursive: true })) {
-    fs.mkdirSync(fullPath, { recursive: true })
+function createFolderIfNotExits(path) {
+  if (!fs.existsSync(path, { recursive: true })) {
+    fs.mkdirSync(path, { recursive: true })
   }
 }
 
-async function saveFile (imagePath, download_response) {
-  createFolderIfNotExits(`${__dirname}/resized/${imagePath.album}`);
-  const imgPath = path.resolve(__dirname, 'resized', imagePath.album, imagePath.photo);
-  const writer = fs.createWriteStream(imgPath)
+function getImagePath(photoFSDetails, folderName) {
+  const album = photoFSDetails.album;
+  // const folderName = 'resized';
+  const albumPath = path.join(__dirname, folderName, album);
 
-  download_response.data.pipe(writer)
+  createFolderIfNotExits(albumPath);
+  const imagePath = path.resolve(albumPath, fileName);
+
+  return imagePath;
+}
+
+async function saveFile (photoFSDetails, response, folderName) {
+  const imagePath = getImagePath(photoFSDetails, folderName)
+  const writer = fs.createWriteStream(imagePath)
+
+  response.data.pipe(writer)
 
   return new Promise((resolve, reject) => {
     writer.on('finish', resolve)
