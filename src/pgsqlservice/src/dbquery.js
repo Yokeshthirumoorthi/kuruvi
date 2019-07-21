83c0cecd9b631afbbfc8574ac0d197c51995b335
yokesh <yokeshthirumoorthi@gmail.com>
2019-07-20 22:50:08 -0700

    add more insert queries

--- src/pgsqlservice/src/dbquery.js
+++ src/pgsqlservice/src/dbquery.js
@@ -22,20 +22,27 @@ const client = new Client({
   port: 5432,
 });
 
+/**
+ * Execute the insert query for the given row values
+ */
+async function insertRow(insertQuery, values) {
+    await client.connect();
+    const res = await client.query(insertQuery, values);
+    await client.end();
+    const id = res.rows[0].id;
+    return id;
+}
 
 /**
  * Inserts data into Album table
  */
 async function albumInsertRow(data) {
-    await client.connect();
 //   const albumId = TODO: Implement uuid for id generation
     const albumName = data.name;
     const albumPath = data.path;
     const values = [albumName, albumPath];
     const query = `INSERT INTO Albums (name, path) VALUES ($1, $2) RETURNING id`; 
-    const res = await client.query(query, values);
-    await client.end();
-    const albumId = res.rows[0].id;
+    const albumId = await insertRow(query, values);
     logger.info(`Successfully inserted row# ${albumId} in album table`);
     return albumId;
 }
@@ -44,16 +51,13 @@ async function albumInsertRow(data) {
  * Inserts data into Photo table
  */
 async function photoInsertRow(data) {
-    await client.connect();
     // const photoId = TODO: Implement uuid for id generation
     const albumId = data.albumId;
     const photoName = data.name;
     const values = [albumId, photoName];
     const query = `INSERT INTO photos (album_id, name) VALUES ($1, $2) RETURNING id`; 
-    const res = await client.query(query, values);
-    await client.end();
-    const photoId = res.rows[0].id;
-    logger.info(`Successfully inserted row# ${photoId} in photo table`);
+    const photoId = await insertRow(query, values);
+    logger.info(`Successully inserted row# ${photoId} in photo table`);
     return photoId;
 }
 
@@ -61,24 +65,57 @@ async function photoInsertRow(data) {
  * Inserts data into exif table
  */
 async function exifInsertRow(data) {
-    await client.connect();
     // const exifId = TODO: Implement uuid for id generation
+    const photoId = data.photoId;
     const make = data.make;
     const model = data.model;
     const createOn= data.createOn;
     const imageWidth= data.imageWidth;
     const imageHeight= data.imageHeight;
-    const values = [make, model, createOn, imageWidth, imageHeight];
-    const query = `INSERT INTO exif (make, model, create_on, img_width, img_height) VALUES ($1, $2, $3, $4, $5) RETURNING id`; 
-    const res = await client.query(query, values);
-    await client.end();
-    const exifId= res.rows[0].id;
+    const values = [photoId, make, model, createOn, imageWidth, imageHeight];
+    const query = `INSERT INTO exif (photo_id, make, model, create_on, img_width, img_height) VALUES ($1, $2, $3, $4, $5) RETURNING id`; 
+    const exifId = await insertRow(query, values);
     logger.info(`Successfully inserted row# ${exifId} in exif table`);
     return exifId;
+}
+
+/**
+ * Inserts data into bounding_boxes table 
+ */
+async function boundingBoxInsertRow(data) {
+    // const boundingBoxId = TODO: Implement uuid for id generation 
+    const photoId = data.photoId;
+    const x = data.x;
+    const y = data.y;
+    const width = data.width;
+    const height = data.height;
+    const values = [photoId, x, y, width, height];
+    const query = `INSERT INTO bounding_boxes (photo_id, x, y, width, height) VALUES ($1, $2, $3, $4, $5) RETURNING id`;
+    const boundingBoxId = await insertRow(query, values);
+    logger.info(`Successfully inserted row# ${boundingBoxId} into bounding_boxes table`);
+    return boundingBoxId;
+}
+
+/**
+ * Inserts data into face table
+ */
+async function faceInsertRow(data) {
+    // const faceId = TODO: Implement uuid for id generation
+    const photoId = data.photoId;
+    const boundingBoxId = data.boundingBoxId;
+    const fileName = data.fileName;
+    const filePath = data.filePath;
+    const values = [photoId, boundingBoxId, fileName, filePath];
+    const query = `INSERT INTO photos (photo_id, boundingBox_id, file_name, file_path) VALUES ($1, $2, $3, $4) RETURNING id`; 
+    const faceId = await insertRow(query, values);
+    logger.info(`Successully inserted row# ${faceId} in face table`);
+    return faceId;
 }
 
 module.exports = {
     albumInsertRow,
     photoInsertRow,
-    exifInsertRow
+    exifInsertRow,
+    boundingBoxInsertRow,
+    faceInsertRow
 }
--- src/pgsqlservice/test/manual-insertquery.js
+++ src/pgsqlservice/test/manual-insertquery.js
@@ -11,6 +11,7 @@ const photoRow = {
 };
 
 const exifRow = {
+    photoId: 2,
     make: 'test_make',
     model: 'test_model',
     createOn: 12345,
@@ -18,10 +19,27 @@ const exifRow = {
     imageHeight: 12345
 };
 
+const boundingBoxRow = {
+    photoId: 2,
+    x: 1,
+    y: 2,
+    width: 12345,
+    height: 12345
+}
+
+const faceRow = {
+    photoId: 2,
+    boundingBoxId: 2, 
+    fileName: 'test_name',
+    filePath: 'test_path'
+}
+
 async function run() {
     // await dbquery.albumInsertRow(albumRow);
     // await dbquery.photoInsertRow(photoRow);
-    await dbquery.exifInsertRow(exifRow);
+    // await dbquery.exifInsertRow(exifRow);
+    // await dbquery.boundingBoxInsertRow(boundingBoxRow);
+    await dbquery.faceInsertRow(faceRow);
 }
 
 run();
