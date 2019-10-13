9376df0f60406517dbcdfcf196e203ee52949e04
yokesh <yokeshthirumoorthi@gmail.com>
2019-10-13 15:35:01 -0700

    build hugo via curl

--- src/frontend/webserver.go
+++ src/frontend/webserver.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "os/exec"
 )
 
+func execute() {
+    out, err := exec.Command("hugo").Output()
+    if err != nil {
+        fmt.Printf("%s", err)
+    }
+    fmt.Println("Command Successfully Executed")
+    output := string(out[:])
+    fmt.Println(output)
+}
+
 func main() {
-	http.HandleFunc("/hello", func(rw http.ResponseWriter, req *http.Request) {
-		name := req.URL.Query().Get("name")
-		rw.Write([]byte(fmt.Sprintf("Hello , %s", name)))
+	http.HandleFunc("/buildhugo", func(rw http.ResponseWriter, req *http.Request) {
+		execute()
 	})
 	http.ListenAndServe(":8079", nil)
 }
